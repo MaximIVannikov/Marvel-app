213add96b4b89a7311f871b6b9eb3089ed59afd0
Maxim Ivannikov <84341083+MaximIVannikov@users.noreply.github.com>
2021-11-13 21:02:08 +0100

    added RandomCharacter

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
-import MarvelService from './services/MarvelService';
+// import MarvelService from './services/MarvelService';
 
-const marvelService = new MarvelService();
+// const marvelService = new MarvelService();
 
-marvelService.getAllCharacters().then(res => (console.log(res)));
+// marvelService.getAllCharacters().then(res => (console.log(res)));
 
-marvelService.getCharacter(1011052).then(res => (console.log(res)));
+// marvelService.getCharacter(1011052).then(res => (console.log(res)));
 
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
+// marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 ReactDOM.render(
   <React.StrictMode>
